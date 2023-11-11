386f3479f0b0d24ad75a5bcafeeefd868213dcf9
MilosMilutinovic01 <milutinovicgit@gmail.com>
2023-11-11 15:59:27 +0100

    initial commit

---
+++ medical-equipment-back-end/src/test/java/com/e2/medicalequipment/MedicalEquipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e2.medicalequipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalEquipmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
