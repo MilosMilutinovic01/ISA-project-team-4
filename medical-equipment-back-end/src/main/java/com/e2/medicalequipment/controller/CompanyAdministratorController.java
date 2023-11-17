a1d6103964d739011af3a3320c7fd0e848e45e98
mareenah <kicanovicmarina@gmail.com>
2023-11-17 21:12:07 +0100

    feat: Added controller, service, repository, dto and model for CompanyAdministrator

---
+++ medical-equipment-back-end/src/main/java/com/e2/medicalequipment/controller/CompanyAdministratorController.java
@@ -0,0 +1,35 @@
+package com.e2.medicalequipment.controller;
+
+import com.e2.medicalequipment.dto.CompanyAdministratorDTO;
+import com.e2.medicalequipment.model.CompanyAdministrator;
+import com.e2.medicalequipment.service.CompanyAdministratorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api/companyAdministrators", produces = MediaType.APPLICATION_JSON_VALUE)
+@CrossOrigin
+public class CompanyAdministratorController {
+    @Autowired
+    private CompanyAdministratorService companyAdministratorService;
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "welcome";
+    }
+
+    @PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CompanyAdministrator> createTest(@RequestBody CompanyAdministratorDTO companyAdministratorDto)  {
+        CompanyAdministrator savedCompanyAdministrator = null;
+        try {System.out.println("Thread id: " + Thread.currentThread().getId());
+            savedCompanyAdministrator = companyAdministratorService.Create(companyAdministratorDto);
+            return new ResponseEntity<CompanyAdministrator>(savedCompanyAdministrator, HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<CompanyAdministrator>(savedCompanyAdministrator, HttpStatus.CONFLICT);
+        }
+    }
+}
