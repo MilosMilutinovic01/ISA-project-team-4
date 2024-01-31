0365ca1496616a313c82d4ac4c22bcb83bf0bcfb
Ana <anaranic01@gmail.com>
2024-01-31 09:31:42 +0100

    fix: fixed test

--- medical-equipment-front-end/src/app/feature-modules/stakeholder/equipment-pickup-qr/equipment-pickup-qr.component.ts
+++ medical-equipment-front-end/src/app/feature-modules/stakeholder/equipment-pickup-qr/equipment-pickup-qr.component.ts
@@ -15,14 +15,14 @@ export class EquipmentPickupQrComponent {
   appointmentId: any;
   appointment: any;
   message: String = '';
-  canPickUp: boolean = true;
+  canPickUp: any;
   user: any;
 
   constructor(
     private service: StakeholderService,
     private authService: AuthService,
     public router: Router
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.authService.user$.subscribe((user) => {
@@ -32,7 +32,6 @@ export class EquipmentPickupQrComponent {
 
   onFileSelected(event: any): void {
     const fileInput = event.target;
-
     if (fileInput.files && fileInput.files.length > 0) {
       this.selectedFile = fileInput.files[0];
       console.log(this.selectedFile?.name);
@@ -71,62 +70,65 @@ export class EquipmentPickupQrComponent {
   isReservationAvailable(): void {
     this.service.isReservationAvailable(this.appointmentId).subscribe(
       (result: boolean) => {
+        const currentTime = new Date();
+        const [year, month, day, hour, minute] = this.appointment.endTime;
+        const endTime = new Date(year, month - 1, day, hour, minute);
         if (!result) {
           this.message = '<br>QR code has already been processed.';
           this.canPickUp = false;
-        } else {
-          const currentTime = new Date();
-          const [year, month, day, hour, minute] = this.appointment.endTime;
-          const endTime = new Date(year, month - 1, day, hour, minute);
-          if (endTime < currentTime) {
-            this.message =
-              '<br>Reservation has expired on ' + endTime.toDateString();
-            this.canPickUp = false;
-            this.processExpiredReservation();
-          } else if (
-            this.appointment.companyAdministrator.id !== this.user.id
-          ) {
-            this.message =
-              '<br>This reservation is assigned to another administrator';
-            this.canPickUp = false;
-          }
+        }
+        else if (endTime < currentTime) {
+          this.message =
+            '<br>Reservation has expired on ' + endTime.toDateString();
+          this.canPickUp = false;
+          this.processExpiredReservation();
+        }
+        else if (this.appointment.companyAdministrator.id !== this.user.id) {
+          this.message =
+            '<br>This reservation is assigned to another administrator';
+          this.canPickUp = false;
+        }
+        else {
+          this.message =
+          '';
+        this.canPickUp = true;
         }
       },
       (error) => {
-        console.error(error);
-      }
+  console.error(error);
+}
     );
   }
 
-  completeReservation(): void {
-    this.service.completeReservation(this.appointment.id).subscribe({
-      next: (result: boolean) => {
-        if (result === true) {
-          alert('Succesfully delivered equipment!');
-          this.router.navigate(['/']);
-        } else {
-          alert('Failed to deliver equipment!');
-        }
-      },
-      error: (error) => {
-        console.log('Error:', error);
-      },
-    });
-  }
+completeReservation(): void {
+  this.service.completeReservation(this.appointment.id).subscribe({
+    next: (result: boolean) => {
+      if (result === true) {
+        alert('Succesfully delivered equipment!');
+        this.router.navigate(['/']);
+      } else {
+        alert('Failed to deliver equipment!');
+      }
+    },
+    error: (error) => {
+      console.log('Error:', error);
+    },
+  });
+}
 
-  processExpiredReservation(): void {
-    this.service.processExpiredReservation(this.appointment.id).subscribe({
-      next: (result: boolean) => {
-        if (result === true) {
-          this.message +=
-            '<br>Customer received 2 penalty points, and the equipment count has been updated.';
-        } else {
-          alert('Failed to process expired reservation!');
-        }
-      },
-      error: (error) => {
-        console.log('Error:', error);
-      },
-    });
-  }
+processExpiredReservation(): void {
+  this.service.processExpiredReservation(this.appointment.id).subscribe({
+    next: (result: boolean) => {
+      if (result === true) {
+        this.message +=
+          '<br>Customer received 2 penalty points';
+      } else {
+        alert('Failed to process expired reservation!');
+      }
+    },
+    error: (error) => {
+      console.log('Error:', error);
+    },
+  });
+}
 }
